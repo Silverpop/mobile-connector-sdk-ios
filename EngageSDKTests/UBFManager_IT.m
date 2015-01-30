f21df188d37db791d29eb4d5b0e691f00e213a70
Lindsay Thurmond <lindsay.thurmond@makeandbuild.com>
2015-01-29 21:12:20 -0500

    checking in unimplemented test to get build working ....

---
+++ EngageSDKTests/UBFManager_IT.m
@@ -0,0 +1,41 @@
+//
+//  UBFManager_IT.m
+//  EngageSDK
+//
+//  Created by Lindsay Thurmond on 1/23/15.
+//  Copyright (c) 2015 Silverpop. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "EngageBaseTest_IT.h"
+#import "UBFManager.h"
+#import "EngageEvent.h"
+
+@interface UBFManager_IT : EngageBaseTest_IT
+
+@end
+
+@implementation UBFManager_IT
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    
+//    [[UBFManager sharedInstance] ]
+    
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
